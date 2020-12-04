78c3b90f90f96fe21f899320a3eae71650a58d31
xiang <479026126@qq.com>
2020-12-04 18:38:07 +0800

    装饰器模式补充

---
+++ src/decorator/JavaIO/Client.java
@@ -0,0 +1,31 @@
+package decorator.JavaIO;
+
+import java.io.*;
+
+/**
+ * @author: xiang
+ * @date: 2020/12/4
+ * @description:
+ */
+public class Client {
+    public static void main(String[] args) {
+        int c;
+        InputStream in = null;
+        try{
+            in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("src/decorator/JavaIO/test.txt")));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char)c);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/decorator/JavaIO/LowerCaseInputStream.java
@@ -0,0 +1,31 @@
+package decorator.JavaIO;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author: xiang
+ * @date: 2020/12/4
+ * @description:
+ */
+public class LowerCaseInputStream extends FilterInputStream {
+    protected LowerCaseInputStream(InputStream in) {
+        super(in);
+    }
+
+    @Override
+    public int read() throws IOException {
+        int c = super.read();
+        return c == -1 ? c : Character.toLowerCase((char)c);
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        int result = super.read(b, off, len);
+        for(int i = off ; i < off+result ; i++) {
+            b[i] = (byte)Character.toLowerCase((char)b[i]);
+        }
+        return result;
+    }
+}
